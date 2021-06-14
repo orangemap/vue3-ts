92834bfc0875400b71e5d1637d007e1afe481637
wangjianwen22 <wangjianwen777@163.com>
2021-06-14 16:41:58 +0800

    no message

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import router from "./router";
 import store from "./store";
 import plugins from "./plugins";
 import "@/type";
-import "./scss/main.scss";
-console.log(1);
-
+import "./scss/main.scss";  
 const app = createApp(App)
   .use(store)
   .use(router)
